c47e5adfca62962f6b74fdea1461860dc48cfd86
Yumin Wu <wuyumin@wuyumin.com>
2018-01-28 03:28:31 +0800

    Modify windows compile script

--- cmd/compile.bat
+++ cmd/compile.bat
@@ -13,46 +13,52 @@ set CGO_ENABLED=0
 set GOOS=windows
 set GOARCH=386
 set GOEXE=.exe
-go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%%GOEXE% %goName%
-upx %execName%-%version%-%GOOS%-%GOARCH%%GOEXE%
+mkdir %execName%-%version%-%GOOS%-%GOARCH%
+go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE% %goName%
+upx %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE%
 @echo.
 
 @echo Compile windows 64 bit...
 set GOOS=windows
 set GOARCH=amd64
 set GOEXE=.exe
-go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%%GOEXE% %goName%
-upx %execName%-%version%-%GOOS%-%GOARCH%%GOEXE%
+mkdir %execName%-%version%-%GOOS%-%GOARCH%
+go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE% %goName%
+upx %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE%
 @echo.
 
 @echo Compile Linux 32 bit...
 set GOOS=linux
 set GOARCH=386
 set GOEXE=
-go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%%GOEXE% %goName%
-upx %execName%-%version%-%GOOS%-%GOARCH%%GOEXE%
+mkdir %execName%-%version%-%GOOS%-%GOARCH%
+go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE% %goName%
+upx %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE%
 @echo.
 
 @echo Compile Linux 64 bit...
 set GOOS=linux
 set GOARCH=amd64
 set GOEXE=
-go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%%GOEXE% %goName%
-upx %execName%-%version%-%GOOS%-%GOARCH%%GOEXE%
+mkdir %execName%-%version%-%GOOS%-%GOARCH%
+go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE% %goName%
+upx %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE%
 @echo.
 
 @echo Compile Mac 32 bit...
 set GOOS=darwin
 set GOARCH=386
 set GOEXE=
-go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%%GOEXE% %goName%
-upx %execName%-%version%-%GOOS%-%GOARCH%%GOEXE%
+mkdir %execName%-%version%-%GOOS%-%GOARCH%
+go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE% %goName%
+upx %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE%
 @echo.
 
 @echo Compile Mac 64 bit...
 set GOOS=darwin
 set GOARCH=amd64
 set GOEXE=
-go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%%GOEXE% %goName%
-upx %execName%-%version%-%GOOS%-%GOARCH%%GOEXE%
+mkdir %execName%-%version%-%GOOS%-%GOARCH%
+go build -ldflags="-s -w" -o %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE% %goName%
+upx %execName%-%version%-%GOOS%-%GOARCH%\%execName%%GOEXE%
 @echo.
