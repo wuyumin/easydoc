2d5b3035872a805059efab22d2a3c5b97a9b769e
Yumin Wu <wuyumin@wuyumin.com>
2018-01-31 22:26:53 +0800

    add function ExistsOrMkdir

--- utils/utils.go
+++ utils/utils.go
@@ -12,3 +12,14 @@ func CheckErr(err error) {
 		os.Exit(1)
 	}
 }
+
+// Is directory exist? Or make.
+func ExistsOrMkdir(dir string) error {
+	//exist?
+	if _, err := os.Stat(dir); err == nil {
+		return nil
+	}
+	//make
+	err := os.MkdirAll(dir, os.ModePerm)
+	return err
+}
